0b9c16e7a67e63dd8473ff759031dfce1a710a91
Fabian Vives <f.vives99@gmail.com>
2020-09-09 13:13:43 -0600

    tc1

--- euler.py
+++ euler.py
@@ -1,8 +1,21 @@
+import math
+
+
 def e_cuadratica(n):
-    # Implemente esta función
-    return 0
+    i=0
+    result = 0
+    while i<=n:
+    	result += 1/math.factorial(i)
+    	i+=1
+    return result
 
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+	euler = 0
+	factorial = 1
+	for i in range(1,n):
+		euler += 1/factorial
+		factorial *= i
+	return euler
+
+
